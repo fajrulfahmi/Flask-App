358bc7f5a5bb9bed215fe111f3fc57380d798159
Fajrul Fahmi <95855377+fajrulfahmi@users.noreply.github.com>
2024-10-21 14:18:36 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,41 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
+import numpy as np
+import pickle
+import os  
 
 app = Flask(__name__)
+
+# Muat model dari file pickle
+model_path = os.path.join('model', 'water_usage_model.pkl')
+with open(model_path, 'rb') as model_file:
+    model = pickle.load(model_file)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def predict_water_consumption():
+    # Ambil data dari POST request dalam format JSON
+    data = request.json
+    waterFlowRate = data['waterFlowRate']
+    temp = data['temp']
+    humidity = data['humidity']
+    numResidents = data['numResidents']
+    specialEvent = data['specialEvent']
+
+    # Siapkan data untuk prediksi
+    input_data = np.array([[waterFlowRate, temp, humidity, numResidents, specialEvent]])
+
+    # Lakukan prediksi
+    predicted_consumption = model.predict(input_data)
+
+    # Format respon dalam JSON
+    response = {
+        'predicted_consumption': predicted_consumption[0],
+        'percent_change_from_previous_day': 5.5  # Misalnya perubahan konsumsi dari hari sebelumnya
+    }
+    return jsonify(response)
 
 
 if __name__ == '__main__':
